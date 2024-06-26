77b4f585111cd899c73e5be2fc2750facbaaf7cb
CamposmDev <camposm.dev@gmail.com>
2024-06-26 16:40:18 -0400

    fix: limit system tray functionality to Windows
    The system tray feature is currently only working on Windows due to issues with Linux distributions. For now, the system tray will only be active on Windows, and the application will only run in the terminal on Linux systems.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
 #!/bin/python3
+import sys
+
 import war
 import signal
 from signal import SIGINT
-import sys
+import time
 
 foxhole: war.Foxhole | None = None
+
 
 def main():
 	global foxhole
 	signal.signal(SIGINT, terminate)
 	foxhole = war.build()
 	foxhole.start()
+	while True:
+		time.sleep(1)
+
 
 def terminate(code, frame):
-	print('Stopping program...')
+	print('Terminating...')
 	global foxhole
 	if foxhole is not None:
 		foxhole.stop()
+	sys.exit(0)
+
 
 if __name__ == "__main__":
 	main()
