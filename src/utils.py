d91404c6bbe52a106af20ebcbc6c4506d6ba7d9d
CamposmDev <camposm.dev@gmail.com>
2024-08-13 18:37:12 -0400

    chore: removed old files

--- src/utils.py
+++
@@ -1,9 +0,0 @@
-from pynput.keyboard import Key, KeyCode
-
-KEYCODE_W = KeyCode.from_char('w')
-KEYCODE_A = KeyCode.from_char('a')
-KEYCODE_S = KeyCode.from_char('s')
-KEYCODE_D = KeyCode.from_char('d')
-
-def is_wasd(key: (Key | KeyCode | None)) -> bool:
-    return key in [KeyCode.from_char(char) for char in 'wasd']
